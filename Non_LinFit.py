a43a116a874209bda3d4ed842ae0ffbecade84bf
mattidler <mattidler@gmail.com>
2017-01-11 18:11:44 -0800

    Add files via upload

---
+++ Non_LinFit.py
@@ -0,0 +1,34 @@
+#!/opt/local/bin/python
+
+import numpy as np
+import numpy.random as ran
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+#Loading Data in
+datax = np.loadtxt("nonLinearFit.dat", np.float64).T[0]
+datay = np.loadtxt("nonLinearFit.dat", np.float64).T[1]
+
+#Setting up the figure parameters
+plt.figure(figsize=(8,6), dpi=80)
+plt.subplot(111)
+plt.scatter(datax, datay, 5, color="blue", label="data")
+
+#Defining Funtion
+def g( x, a, b, c, d ):
+	return a * np.cos(2.5*x - 0.5*b) + c * np.sin(2.5*x - x*d) 
+
+(popt, pcov) = curve_fit(g, datax, datay, p0=None,sigma=None)
+
+print(popt)
+
+#Setting up curve fit
+xfitted = datax
+yfitted = g(xfitted,popt[0],popt[1],popt[2],popt[3])
+plt.plot(xfitted, yfitted, color="red", linewidth=1.0, linestyle="-", label="fitted")
+
+#Plotting
+plt.legend(loc='lower left', frameon=False)
+plt.savefig("NonLinFit.pdf", dpi=72)
+
+plt.show()
